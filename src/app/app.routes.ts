450d120ecd21e6d53f60e73615d5367d78589066
Carofort <dayannafort@gmail.com>
2026-05-20 10:51:02 -0500

    feat: formulario signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomePage } from './features/home/pages/home-page/home-page';
 import { StudentsPage } from './features/students/pages/students-page/students-page';
 import { StudentDetailPage } from './features/students/pages/student-detail-page/student-detail-page';
 import { LayoutsPage } from './features/home/pages/layouts-page/layouts-page';
+import { SignupPage } from './features/signup-page/signup-page';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
     {path: 'students', component: StudentsPage},
     {path: 'layouts', component: LayoutsPage},
     {path: 'students/:id', component: StudentDetailPage},
+    {path: 'signup', component: SignupPage},
     //redireccionamiento
     {path: '**', redirectTo: ''}
 ];
---
+++ src/app/features/signup-page/signup-page.ts
@@ -0,0 +1,60 @@
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { passwordMatchValidator } from './validators/password-match.validator';
+import { Router } from '@angular/router';
+import { emailUniqueValidator } from './validators/email-unique.validator';
+
+@Component({
+  selector: 'app-signup-page',
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class SignupPage {
+  //emailControl= new FormControl(
+  //  '',
+  //   [Validators.required, Validators.email], //Validadores sincronos
+  //  );
+
+  //get email() {
+  //  return this.emailControl;
+  //}
+
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email], [emailUniqueValidator()]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: passwordMatchValidator },
+  );
+
+  get email() {
+    return this.form.get('email')!;
+  }
+  get password() {
+    return this.form.get('password')!;
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword')!;
+  }
+
+  private router = inject(Router);
+
+  onSubmit() {
+    if (this.form.invalid) {
+      // Marcar todos los campos como touched para mostrar errores
+      this.form.markAllAsTouched();
+
+      return;
+    }
+
+    console.log('Datos del formulario:', this.form.value);
+
+    // Por ahora, navegar a home
+    this.router.navigate(['/']);
+  }
+}
